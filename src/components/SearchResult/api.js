a9445c7df866f116085d8f268fbba3b71365cfd7
Victoria Liu <66372123+victoria-lyq@users.noreply.github.com>
2021-12-05 00:10:30 -0800

    try yelp fusion

--- src/components/SearchResult/api.js
+++ src/components/SearchResult/api.js
@@ -1,6 +1,10 @@
 import {API_BASE_URL, BEARER_TOKEN} from './config';
 import import queryString from 'query-string';
 
+//
+// example url
+// https://api.yelp.com/v3/businesses/search?term=restaurants&location=SanFrancisco
+// term=restaurants&location=seattle
 export function get(path, queryParams) {
     const query = queryString.stringify(queryParams);
     return fetch(`${API_BASE_URL}${path}?${query}`, {
