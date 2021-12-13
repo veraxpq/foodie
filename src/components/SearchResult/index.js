318cc5fadca11f5052a94a682c090d04ec9ff1c9
Jarvis Ren <jarvisren@Jarviss-MacBook-Pro.local>
2021-12-13 14:57:50 -0800

    wip search

--- src/components/SearchResult/index.js
+++ src/components/SearchResult/index.js
@@ -7,14 +7,16 @@ import {useSelector} from "react-redux";
 import SearchBar from "../SearchBar";
 import {getRestaurantByLocation, getRestaurantByTermAndLocation} from "../../services/searchRestaurantsServices";
 const SearchResult = () => {
-    const queryString = window.location.search;
-    console.log("search reult url",queryString);
-    const urlParams = new URLSearchParams(queryString);
-    const term = urlParams.get('term');
-    const location = urlParams.get('cityName');
+    // const queryString = window.location.search;
+    // console.log("search reult url",queryString);
+    // const urlParams = new URLSearchParams(queryString);
+    // const term = urlParams.get('term');
+    // const location = urlParams.get('cityName');
     // const params = useParams();
     // const term = params.term;
     // const location = params.location;
+    const term = localStorage.getItem('term');
+    const location = localStorage.getItem('location');
     console.log("search result index getting term and location", term, location);
     return(
         <div>
