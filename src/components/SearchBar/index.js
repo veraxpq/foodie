b995f89d49931ae18a778c95bce23fe62dc8c4f4
veraxpq <634505024@qq.com>
2021-12-13 14:58:11 -0800

    save

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -8,9 +8,12 @@ const SearchBar=()=>{
     const [location, setLocation] = useState(params.location|| '');
     console.log("user inputed search pair at search bar", {term, location})
     function submit(e) {
+        localStorage.setItem("location", event.target.value)
+        localStorage.setItem("term", event.target.value)
+
         if(term && location) {
-            const encodedTerm = encodeURI(term);
-            const encodedLocation = encodeURI(location);
+            const encodedTerm = encodeURI(localStorage.getItem("term"));
+            const encodedLocation = encodeURI(localStorage.getItem("location"));
             console.log("user searched pair", {encodedTerm, encodedLocation})
             navigate(`/search?term=${encodedTerm}&cityName=${encodedLocation}`);
             // navigate(`/search/term=${encodedTerm}&cityName=${encodedLocation}`);
@@ -29,17 +32,16 @@ const SearchBar=()=>{
             <input className="form-control me-sm-2"
                    type="text"
                    placeholder="What do you want to eat?"
-                   value={term}
+                   value={localStorage.getItem("term")}
                    onChange={(event) => setTerm(event.target.value)}></input>
             <input
                 className="form-control me-sm-2"
                 type="text"
                 placeholder="Where"
-                value={location}
+                value={localStorage.getItem("location")}
                 onChange={(event)=>setLocation(event.target.value)}></input>
             <button className="f-home-search-btn"
-                                       type="submit"
-                                       onClick={submit}>Search</button>
+                    onClick={submit}>Search</button>
         </form>
     )
 }
