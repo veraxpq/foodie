c12fb482a20699319fe0e11ba3bbf206bb1dd73b
veraxpq <634505024@qq.com>
2021-12-14 11:57:38 -0800

    add / route

--- src/App.js
+++ src/App.js
@@ -45,7 +45,8 @@ function App() {
                     <Route path={"/register"} element={<Register/>}/>
                     <Route path={"/login"} element={<Login/>}/>
                     <Route path={"/findPassword"} element={<ForgotPassword/>}/>
-                    <Route path={"/", "/home"} element={<HomePage/>}/>
+                    <Route path={"/home"} element={<HomePage/>}/>
+                    <Route path={"/"} element={<HomePage/>}/>
                     <Route path={"/registerForBusiness"} element={<RegisterPageForBusiness/>}/>
                     <Route path={"/personal_profile"} element={<PersonalProfileScreen/>}/>
                     <Route path={"/saved_restaurants"} element={<SavedRestaurantsScreen/>}/>
