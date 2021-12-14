f4a8bb9308eaab4bf24b31ed0213bfbc7ba1e06d
Joe112-code <62823935+Joe112-code@users.noreply.github.com>
2021-12-13 22:03:09 -0500

    test

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -81,16 +81,16 @@ const TopBarComponent = () => {
                                 {/*        </div>*/}
                                 {/*    </div>*/}
                                 {/*</Link>*/}
-                                <Link to={"/search/restaurant/seattle"} className={"list-group-item dropdown-item"}>
-                                    <div className="row">
-                                        <div className="col-2">
-                                            <i className="fas fa-hashtag"></i>
-                                        </div>
-                                        <div className="d-none d-xl-block col-10">
-                                            Explore
-                                        </div>
-                                    </div>
-                                </Link>
+                                {/*<Link to={"/search/restaurant/seattle"} className={"list-group-item dropdown-item"}>*/}
+                                {/*    <div className="row">*/}
+                                {/*        <div className="col-2">*/}
+                                {/*            <i className="fas fa-hashtag"></i>*/}
+                                {/*        </div>*/}
+                                {/*        <div className="d-none d-xl-block col-10">*/}
+                                {/*            Explore*/}
+                                {/*        </div>*/}
+                                {/*    </div>*/}
+                                {/*</Link>*/}
                                 <div onClick={submitProfile} className={"list-group-item dropdown-item f-hand-cursor"}>
                                     <div className="row">
                                         <div className="col-2">
